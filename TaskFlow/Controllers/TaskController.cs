fd8c3c6ee0eb7c6fc314509026207706d9002960
adizeus5798 <adarshharakangi_2020@depauw.edu>
2025-01-10 17:42:10 -0600

    TKFL-3 Creating Task Page and Adding Create Functionality

---
+++ TaskFlow/Controllers/TaskController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskFlow.Data;
+using TaskFlow.Models;
+
+namespace TaskFlow.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly TaskFlowDbContext _context;
+
+        public TaskController(TaskFlowDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var tasks = _context.TaskItems.ToList();
+            return View(tasks);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TaskItemViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var task = new TaskItem
+                {
+                    Title = model.Title,
+                    Description = model.Description,
+                    IsCompleted = false
+                };
+
+                _context.TaskItems.Add(task);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ TaskFlow/Models/TaskItemViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskFlow.Models
+{
+    public class TaskItemViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
