b8e055cdad1b539af38f02907b9f24fa2a56babe
adizeus5798 <adarshharakangi_2020@depauw.edu>
2025-01-15 11:59:50 -0600

    TKFL-4 Edit and Delete Task UI + Backend

--- TaskFlow/Controllers/TaskController.cs
+++ TaskFlow/Controllers/TaskController.cs
@@ -43,5 +43,69 @@ namespace TaskFlow.Controllers
             }
             return View(model);
         }
+
+        public IActionResult Edit(int id)
+        {
+            var task = _context.TaskItems.Find(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new TaskItemViewModel
+            {
+                Title = task.Title,
+                Description = task.Description
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, TaskItemViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var task = _context.TaskItems.Find(id);
+                if (task == null)
+                {
+                    return NotFound();
+                }
+
+                task.Title = model.Title;
+                task.Description = model.Description;
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var task = _context.TaskItems.Find(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return View(task);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var task = _context.TaskItems.Find(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            _context.TaskItems.Remove(task);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
